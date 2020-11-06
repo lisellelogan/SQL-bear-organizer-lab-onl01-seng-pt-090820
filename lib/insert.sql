c4d1735e35f60a0be6030aebad39cde58d40967c
lisellelogan <liselle.logan@yahoo.com>
2020-11-05 21:08:08 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, temperament, alive) VALUES ("Mr. Chocolate", 7, "M", "chill", 1);
+INSERT INTO bears (name, age, gender, temperament, alive) VALUES ("Rowdy", 7, "M", "hyper", 1);
+INSERT INTO bears (name, age, gender, temperament, alive) VALUES ("Tabitha", 12, "M", "chill", 0);
+INSERT INTO bears (name, age, gender, temperament, alive) VALUES ("Sergeant Brown", 10, "F", "hyper", 1);
+INSERT INTO bears (name, age, gender, temperament, alive) VALUES ("Melissa", 3, "F", "anxious", 1);
+INSERT INTO bears (name, age, gender, temperament, alive) VALUES ("Grinch", 13, "M", "chill", 0);
+INSERT INTO bears (name, age, gender, temperament, alive) VALUES ("Wendy", 7, "F", "anxious", 1);
+INSERT INTO bears (name, age, gender, temperament, alive) VALUES (NULL, 6, "M", "agressive", 1);
+
